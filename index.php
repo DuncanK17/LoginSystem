3244d6af6b21495768e25391d68591d3aa1804d0
DuncanK17 <31687727+DuncanK17@users.noreply.github.com>
2019-06-09 13:59:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,20 +14,13 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/admin.css">
-    <title>Mii-Fit Admin</title>
+
+    <title>Logged-in page</title>
 </head>
+    
 <body>
-
-<?php require_once '../includes/nav-admin.html'?>
-
-    <div class="main">
-        <h1>Welkom</h1>
-
-        <p>
-            Hier kunt u afspraken bekijken en tijden inplannen.
-        </p>
-    </div>
-
+    
+    <h1>Welcome</h1>
+    
 </body>
 </html>
